dd44160690823fae72c2f5081a16f9cd5d465a39
sofiaRubie <sofia.rubie.garcia@est.una.ac.cr>
2024-11-21 21:40:50 -0600

    cors
    cors

--- netlify/functions/headerCORS.js
+++ netlify/functions/headerCORS.js
@@ -1,14 +1,14 @@
-// login.js (Netlify Function)
-const handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Allow all origins, or replace '*' with the specific domain (e.g., 'http://localhost:5173')
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Methods allowed
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
-    },
-    body: JSON.stringify({ message: 'Login successful!' }),
+  // login.js (Netlify Function)
+  const handler = async (event, context) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow all origins, or replace '*' with the specific domain (e.g., 'http://localhost:5173')
+        'Access-Control-Allow-Methods': 'OPTIONS,GET, POST, PUT, DELETE', // Methods allowed
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
+      },
+      body: JSON.stringify({ message: 'Login successful!' }),
+    };
   };
-};
 
-exports.handler = handler;
+  exports.handler = handler;
